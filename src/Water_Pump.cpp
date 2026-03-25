2df879b5f2f9831c00c20ce78cdef408b29b741f
act109 <act109@case.edu>
2026-03-24 21:00:56 -0400

    Create Water_Pump.cpp

---
+++ src/Water_Pump.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+// Define pump pins
+const int motorB_1A = 33;
+const int motorB_2A = 15;
+
+void setup() {
+
+  pinMode(motorB_1A, OUTPUT);  // set pump pin 1 as output
+  pinMode(motorB_2A, OUTPUT);  // set pump pin 2 as output
+
+  digitalWrite(motorB_1A, HIGH);
+  digitalWrite(motorB_2A, LOW);
+
+  delay(5000);// delay 5 seconds
+
+  digitalWrite(motorB_1A, LOW);  // turn off pump
+  digitalWrite(motorB_2A, LOW);
+
+}
+
+void loop() {
+
+}
